f854686329e974d476bc2625dae2c32a931e4e6d
William Cheung <williamct.cheung@gmail.com>
2016-07-12 08:49:50 +0100

    Moved data from views to controllers in dog-list page

--- app_server/controllers/dogs.js
+++ app_server/controllers/dogs.js
@@ -1,6 +1,25 @@
 module.exports.lostDogList = function(req, res, next) {
   res.render('lost-dog-list', {
-    title: 'fetch'
+    title: 'fetch - find lost dogs near you',
+    pageHeader: {
+      title: 'fetch',
+      strapline: 'Helping find lost dogs near you.'
+    },
+    dogs: [{
+      name:'Fido',
+      lastSeenDate: '30th June 2016',
+      lastSeenDistance: '931m',
+      image: '/images/chow-chow.jpg',
+      summaryText: 'Last seen near Glasgow\'s George Square, Fido is a friendly, fluffy, four year-old chow-chow who loves to play fetch.',
+      tags: ['Aggressive!']
+    }, {
+      name:'Growlie',
+      lastSeenDate: '4th July 2016',
+      lastSeenDistance: '6.31km',
+      image: '/images/growlie1.jpg',
+      summaryText: 'Last seen on Cinnabar Island, Growlie is a friendly and loyal level-13 fire Pokémon who was ruthlessly taken from his Trainer by the evil Team Rocket.',
+      tags: ['Friendly', 'Loyal']
+    }]
   });
 };
 
